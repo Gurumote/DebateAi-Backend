a4e0916db40848cf043e2eaaecd3219ff1f4637e
Gurumote <gurumote58@gmail.com>
2026-03-09 12:44:20 +0530

    Auth Request And Response

---
+++ src/main/java/org/gam/dedatebackend/Model/AuthRequest.java
@@ -0,0 +1,14 @@
+package org.gam.dedatebackend.Model;
+
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter@Setter
+public class AuthRequest {
+    private String email;
+    private String password;
+}
