7d0423be93720b0247203fbcefe657c0500ea8d6
Gurumote <gurumote58@gmail.com>
2026-03-09 12:45:50 +0530

    Main file

---
+++ src/main/java/org/gam/dedatebackend/DedateBackendApplication.java
@@ -0,0 +1,13 @@
+package org.gam.dedatebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DedateBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DedateBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/gam/dedatebackend/DedateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gam.dedatebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DedateBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
