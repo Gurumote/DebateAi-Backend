b493b4a31f26c1a03c72349caa11607e3d3262ac
Gurumote <gurumote58@gmail.com>
2026-03-14 00:41:43 +0530

    ContestRepo

---
+++ src/main/java/org/gam/dedatebackend/Repo/ContestRepo.java
@@ -0,0 +1,7 @@
+package org.gam.dedatebackend.Repo;
+
+import org.gam.dedatebackend.Model.Contest.ContestRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContestRepo extends JpaRepository<ContestRoom,Long> {
+}
