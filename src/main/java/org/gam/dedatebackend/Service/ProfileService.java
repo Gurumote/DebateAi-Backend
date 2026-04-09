38ae3af6431b59f11c465ea387d73838a1e8ba8f
Gurumote <gurumote58@gmail.com>
2026-04-10 01:15:29 +0530

    Update

--- src/main/java/org/gam/dedatebackend/Service/ProfileService.java
+++ src/main/java/org/gam/dedatebackend/Service/ProfileService.java
@@ -1,6 +1,6 @@
 package org.gam.dedatebackend.Service;
 
-import org.gam.dedatebackend.Model.ProfileRequest;
+import org.gam.dedatebackend.Model.Request.ProfileRequest;
 import org.gam.dedatebackend.Model.ProfileResponse;
 
 public interface ProfileService {
