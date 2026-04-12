00a2fb0b27d6aa8224d4e6c6bc0cc79bea76d97f
Gurumote <gurumote58@gmail.com>
2026-04-12 22:29:35 +0530

    Added Required JPA data handling Method for ORM Operation

--- src/main/java/org/gam/dedatebackend/Repo/RoomParticipantRepo.java
+++ src/main/java/org/gam/dedatebackend/Repo/RoomParticipantRepo.java
@@ -2,6 +2,18 @@ package org.gam.dedatebackend.Repo;
 
 import org.gam.dedatebackend.Model.Contest.RoomParticipant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import java.util.List;
+import java.util.Optional;
+
+@EnableJpaRepositories
 public interface RoomParticipantRepo extends JpaRepository<RoomParticipant,Long> {
+    Boolean existsByContestRoom_IdAndUserIdAndLeftAtIsNull(String roomId, Long userId);
+
+    Optional<RoomParticipant> findTopByContestRoom_IdAndUserIdAndLeftAtIsNullOrderByJoinedAtDesc(String roomId, Long userId);
+
+    long countByContestRoom_IdAndLeftAtIsNull(String roomId);
+
+    List<RoomParticipant> findByContestRoom_IdAndLeftAtIsNull(String roomId);
 }
