e2b04cad61cf98af8a3c840b9f049f64986f918c
Gurumote <gurumote58@gmail.com>
2026-04-20 00:19:53 +0530

    AuthChannelInterceptor Added

--- src/main/java/org/gam/dedatebackend/Config/WebSocketConfig.java
+++ src/main/java/org/gam/dedatebackend/Config/WebSocketConfig.java
@@ -1,7 +1,10 @@
 package org.gam.dedatebackend.Config;
 
 
+import lombok.RequiredArgsConstructor;
+import org.gam.dedatebackend.Service.Componet.AuthChannelInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -9,12 +12,13 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
+    private final AuthChannelInterceptor  authChannelInterceptor;
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         //client to direct server
-        registry.setApplicationDestinationPrefixes("/api");//all the request coming from the api goes to controller not to handler
+        registry.setApplicationDestinationPrefixes("/api1");//all the request coming from the api goes to Messagemaping handler
         //simple Broker
         registry.enableSimpleBroker("/broker1","/broker2");
 
@@ -31,7 +35,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("http://localhost:5173", "http://localhost:3000")
-                .withSockJS();
+                .setAllowedOriginPatterns("*");
+    }
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(authChannelInterceptor);
     }
 }
